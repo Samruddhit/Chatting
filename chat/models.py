bf933a89cc82ed0b41c362d555fb55d60901acbc
Samruddhi <samruddhi.thakur10@gmail.com>
2019-04-20 17:25:03 -0400

    Schema added

--- chat/models.py
+++ chat/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime, timedelta
 
-from datetime import datetime,timedelta
+
 # Create your models here.
-
 
 
 class UserProfile(models.Model):
@@ -13,22 +13,30 @@ class UserProfile(models.Model):
     )
     user = models.OneToOneField(User, related_name='user_profile', on_delete=models.CASCADE)
 
-
     def __str__(self):
         return self.user.email
 
+
 class Chatroom(models.Model):
     name = models.CharField(max_length=40, blank=False)
-    active = models.BooleanField(default=False)
-    chatowner = models.ManyToManyRel( related_name='owner_name',to=User , field=User.email)
-    participants = models.ForeignKey(User, related_name="user_of_chat", on_delete=models.CASCADE)
     start_time = models.DateTimeField(default=datetime.now)
-    #
+
     # def addSecs(secs):
     #     tm = datetime.now()
     #     fulldate = tm + timedelta(seconds=secs)
     #     return fulldate.time()
 
-    #end_time = models.DateTimeField(default=addSecs(3600))
+    end_time = models.DateTimeField(default=datetime.now)
+
     def __str__(self):
         return str(self.name)
+
+
+class Message(models.Model):
+    created_at = models.DateTimeField(default=datetime.now)
+    messages = models.CharField(max_length=1000)
+    user_id = models.ForeignKey(to=UserProfile, on_delete=models.CASCADE)
+    chat_id = models.ForeignKey(to=Chatroom, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.chat_id + self.user_id)
